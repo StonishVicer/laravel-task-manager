63faab2da6141fb483cf6ae0dffdd89516793532
StonishVicer <samuelpena8220@gmail.com>
2026-02-02 11:23:46 -0400

    feat: create Task model with statuses

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+
+    public const STATUS_PENDING = 'pending';
+    public const STATUS_COMPLETED = 'completed';
+
+    public static function statuses(): array
+    {
+        return [
+            self::STATUS_PENDING,
+            self::STATUS_COMPLETED,
+        ];
+    }
 }
