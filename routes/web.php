23a47f4593bf4776cd7d53361a9e719045fab06f
StonishVicer <samuelpena8220@gmail.com>
2026-02-02 11:29:57 -0400

    feat: configure task routes and confirm resource routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect()->route('tasks.index'));
+
+Route::resource('tasks', TaskController::class)->except(['show']);
+
+Route::patch('tasks/{task}/toggle-status', [TaskController::class, 'toggleStatus'])
+    ->name('tasks.toggle-status');
